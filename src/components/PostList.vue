65716a4e4d16844f9159feb1ee242d565c69b4c2
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-03-13 21:14:07 +0100

    render posts with SinglePost component

---
+++ src/components/PostList.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="post-list">
+        <div v-for="post in posts" :key="post.id">
+            <SinglePost :post="post"/>
+        </div>
+    </div>
+</template>
+
+<script>
+import { onMounted, onUnmounted, onUpdated } from 'vue'
+    import SinglePost from './SinglePost.vue'
+
+    export default {
+        props: ['posts'],
+        components: {
+            SinglePost
+        },
+        setup(props) {
+            onMounted(() => console.log('Component Mounted'))
+            onUnmounted(() => console.log('Component Unmounted'))
+            onUpdated(() => console.log('Component Updated'))
+        }
+    }
+
+</script>
+
+<style>
+
+</style>
